c5e0db17bb2e2bccd9a927d4f7255982b5b77aba
lusen <lusen01@163.com>
2018-11-15 17:04:05 +0800

    test

---
+++ sparkmall-common/src/main/scala/com/atguigu/sparkmall0529/common/model/CityInfo.scala
@@ -0,0 +1,8 @@
+package com.atguigu.sparkmall0529.common.model
+
+case class CityInfo (city_id:Long,
+                     city_name:String,
+                     area:String
+                    ){
+
+}
